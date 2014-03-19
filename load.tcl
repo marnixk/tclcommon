f3027cf514ae3f9230e16cbec039ca3f8865d0a0
Marnix <m.cook@auckland.ac.nz>
2014-03-19 16:27:35 +1300

    Created simple module encapsulation mechanism

---
+++ load.tcl
@@ -0,0 +1,5 @@
+package provide tclcommon 1.0
+
+set pkg_path [file dirname [info script]]
+
+source "$pkg_path/encapsulation.tcl"
---
+++ pkgIndex.tcl
@@ -0,0 +1,2 @@
+package ifneeded tclcommon 1.0 [list source [file join $dir "load.tcl"]]
+
